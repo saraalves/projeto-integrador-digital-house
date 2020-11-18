d70a5c22ebbbcaadc04c04c8ea4f7f0045c19c17
Jenifer Soares <jey.souza11@gmail.com>
2020-11-17 21:13:40 -0300

    Adiciona Dialog da tela de Alterar Senha

--- MarvelApp/app/src/main/java/com/example/marvelapp/alterar_senha/AlterarSenhaActivity.kt
+++ MarvelApp/app/src/main/java/com/example/marvelapp/alterar_senha/AlterarSenhaActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
+import androidx.appcompat.app.AlertDialog
 import com.example.marvelapp.R
 
 class AlterarSenhaActivity : AppCompatActivity() {
@@ -15,8 +16,21 @@ class AlterarSenhaActivity : AppCompatActivity() {
 
         val toolbar = findViewById<Toolbar>(R.id.toolbarAlterarSenha)
         toolbar.setNavigationOnClickListener{
-            finish()
+            showDialog()
         }
+    }
+
+    private fun showDialog(){
+        AlertDialog.Builder(this)
+            .setTitle("Confirmar cancelamento?")
+            .setMessage("Ao cancelar essa ação, seus dados cadastrais não serão alterados.")
+            .setPositiveButton("CONFIRMAR"){dialog, which ->
+                finish()
+            }
+            .setNegativeButton("CONTINUAR EDITANDO"){dialog, which ->
+
+            }
+            .show()
     }
 
 }
