5350b053df9a171110e831430aeb626f16b95b49
SARA ALVES <33603099+saraalves@users.noreply.github.com>
2020-11-21 20:03:21 -0300

    Tela de cadastro com navegação simples (#6)
    * Navegação dos cards e avatar
    
    * Navegação dos cards e avatar

---
+++ MarvelApp/app/src/main/java/com/example/marvelapp/view/activity/CadastroActivity.kt
@@ -0,0 +1,18 @@
+package com.example.marvelapp.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.marvelapp.R
+import com.google.android.material.appbar.MaterialToolbar
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+
+        val toolbarCadastro = findViewById<MaterialToolbar>(R.id.toolbarCadastro)
+        toolbarCadastro.setOnClickListener{
+            onBackPressed()
+        }
+    }
+}
