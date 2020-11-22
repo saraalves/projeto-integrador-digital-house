452751a3a8a28f0185e7ffa7787ce78e071d2693
SARA ALVES <33603099+saraalves@users.noreply.github.com>
2020-11-22 17:27:58 -0300

    Feature/ajustes navegacao (#10)
    * ajustes de navegação
    
    * ajustes de navegação
    
    * ajustes de navegação
    
    * ajustes de toasts
    
    * ajustes toasts
    
    * ajuste no card da home
    
    * ajuste no card da home
    
    * ajuste no card da home

--- MarvelApp/app/src/main/java/com/example/marvelapp/login/LoginActivity.kt
+++ MarvelApp/app/src/main/java/com/example/marvelapp/login/LoginActivity.kt
@@ -3,8 +3,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window.FEATURE_NO_TITLE
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.example.marvelapp.R
+import com.example.marvelapp.alterarsenha.AlterarSenhaActivity
+import com.example.marvelapp.cadastro.CadastroActivity
 import com.example.marvelapp.home.view.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
@@ -15,8 +19,37 @@ class LoginActivity : AppCompatActivity() {
 
         val buttonLogin = findViewById<TextView>(R.id.btnLogin)
         buttonLogin.setOnClickListener{
+            Toast.makeText(this, "Campos vazios", Toast.LENGTH_SHORT).show()
             val intent = Intent(this@LoginActivity, HomeActivity::class.java )
             startActivity(intent)
         }
+
+        val textAlterarSenha = findViewById<TextView>(R.id.btnEsqueciSenha)
+        textAlterarSenha.setOnClickListener {
+            val intent = Intent(this@LoginActivity, AlterarSenhaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val textIrProCadastro = findViewById<TextView>(R.id.btnNaoTenhoCadastro)
+        textIrProCadastro.setOnClickListener {
+            val intent = Intent(this@LoginActivity, CadastroActivity::class.java)
+            startActivity(intent)
+        }
+
+        val imageFacebook = findViewById<ImageView>(R.id.imgLoginFacebook)
+        imageFacebook.setOnClickListener {
+            Toast.makeText(this, "Logar com Facebook", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val imageGoogle = findViewById<ImageView>(R.id.imgLoginGoogle)
+        imageGoogle.setOnClickListener {
+            Toast.makeText(this, "Logar com Google", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
--- MarvelApp/app/src/main/java/com/example/marvelapp/perfil/PerfilFragment.kt
+++ MarvelApp/app/src/main/java/com/example/marvelapp/perfil/PerfilFragment.kt
@@ -1,11 +1,18 @@
 package com.example.marvelapp.perfil
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import com.example.marvelapp.R
+import com.example.marvelapp.alterarsenha.AlterarSenhaActivity
+import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.fragment_perfil.*
+
 class PerfilFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -13,6 +20,21 @@ class PerfilFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnAlterarSenha = view.findViewById<TextView>(R.id.changePassword)
+        btnAlterarSenha.setOnClickListener {
+            val intent = Intent(view.context, AlterarSenhaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnSalvar = view.findViewById<MaterialButton>(R.id.btnSalvarPerfil)
+        btnSalvar.setOnClickListener {
+            Toast.makeText(view.context, "Dados salvos com sucesso", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
