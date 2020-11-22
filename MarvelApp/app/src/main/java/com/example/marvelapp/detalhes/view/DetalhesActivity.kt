e77a0a8d817af0fc75b44b45dc175445741b75f4
Sara Alves <saraalves14@hotmail.com>
2020-11-22 13:14:42 -0300

    ajustes toasts

--- MarvelApp/app/src/main/java/com/example/marvelapp/detalhes/view/DetalhesActivity.kt
+++ MarvelApp/app/src/main/java/com/example/marvelapp/detalhes/view/DetalhesActivity.kt
@@ -1,17 +1,33 @@
 package com.example.marvelapp.detalhes.view
 
+import android.annotation.SuppressLint
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.Toast
 import com.example.marvelapp.R
 import kotlinx.android.synthetic.main.activity_detalhes.*
 
 class DetalhesActivity : AppCompatActivity() {
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
 
         topAppBar.setOnClickListener {
             onBackPressed()
+        }
+
+        val iconShare = findViewById<View>(R.id.share)
+        iconShare.setOnClickListener {
+            Toast.makeText(this, "Compartilhando nas redes sociais", Toast.LENGTH_SHORT).show()
+        }
+
+        val iconFavorite = findViewById<View>(R.id.favorite)
+        iconFavorite.setOnClickListener {
+            Toast.makeText(this, "Favorito removido", Toast.LENGTH_SHORT).show()
         }
     }
 }
