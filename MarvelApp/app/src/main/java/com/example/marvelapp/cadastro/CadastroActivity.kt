41262bd44a3b151983dcc8f6d1fca33b0d5cc300
Sara Alves <saraalves14@hotmail.com>
2020-11-25 18:47:23 -0300

    ajustes

--- MarvelApp/app/src/main/java/com/example/marvelapp/cadastro/CadastroActivity.kt
+++ MarvelApp/app/src/main/java/com/example/marvelapp/cadastro/CadastroActivity.kt
@@ -3,14 +3,19 @@ package com.example.marvelapp.cadastro
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.marvelapp.R
 import com.example.marvelapp.home.view.HomeActivity
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.dialog_confirmacao.view.*
 
 class CadastroActivity : AppCompatActivity() {
+
+    private var alertDialog: AlertDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
@@ -30,15 +35,17 @@ class CadastroActivity : AppCompatActivity() {
     }
 
     private fun showDialog() {
-        AlertDialog.Builder(this)
-            .setTitle("Confirmar cancelamento?")
-            .setMessage("Ao cancelar essa ação, seus dados cadastrais não serão alterados.")
-            .setPositiveButton("CONFIRMAR") { dialog, which ->
-                finish()
-            }
-            .setNegativeButton("CONTINUAR EDITANDO") { dialog, which ->
 
-            }
-            .show()
+        val dialogBuilder = AlertDialog.Builder(this@CadastroActivity)
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_confirmacao, null, false)
+        dialogBuilder.setView(dialogView)
+
+        dialogView.btnContinuarEditando.setOnClickListener { alertDialog?.dismiss() }
+
+        dialogView.btnConfirmar.setOnClickListener { finish() }
+
+        alertDialog = dialogBuilder.create()
+        alertDialog?.window!!.setBackgroundDrawableResource(android.R.color.transparent)
+        alertDialog?.show()
     }
 }
