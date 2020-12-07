63e8103bac9bb0257f463f32a8f186c48d3fb3cd
Jenifer Soares <jey.souza11@gmail.com>
2020-12-07 19:06:06 -0300

    Adciona transição de informações

--- MarvelApp/app/src/main/java/com/example/marvelapp/detalhes/view/DetalhesActivity.kt
+++ MarvelApp/app/src/main/java/com/example/marvelapp/detalhes/view/DetalhesActivity.kt
@@ -7,8 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.example.marvelapp.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detalhes.*
 
 class DetalhesActivity : AppCompatActivity() {
@@ -16,6 +19,17 @@ class DetalhesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
+
+        val nome = intent.getStringExtra("NOME")
+        val descricao = intent.getStringExtra("DESCRIÇÃO")
+        val imagem = intent.getStringExtra("IMAGEM")
+
+        if(descricao.isNullOrEmpty()){
+            findViewById<TextView>(R.id.txtDescricao).text = "Description not avaliable"
+        } else findViewById<TextView>(R.id.txtDescricao).text = descricao
+
+        findViewById<TextView>(R.id.txtNomePersonagemDetail).text = nome
+        Picasso.get().load(imagem).into(findViewById<ImageView>(R.id.imgPersonagemDetail))
 
         topAppBar.setOnClickListener {
             onBackPressed()
