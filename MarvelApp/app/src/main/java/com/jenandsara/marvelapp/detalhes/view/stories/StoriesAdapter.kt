ddb9456126642410b4c6e42f4a50a5bd114564e6
SARA ALVES <33603099+saraalves@users.noreply.github.com>
2021-01-05 14:21:39 -0300

    Feature/new plus dois (#24)
    * new plus 2
    
    * new plus 2

---
+++ MarvelApp/app/src/main/java/com/jenandsara/marvelapp/detalhes/view/stories/StoriesAdapter.kt
@@ -0,0 +1,26 @@
+package com.jenandsara.marvelapp.detalhes.view.stories
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jenandsara.marvelapp.R
+import com.jenandsara.marvelapp.stories.model.StoriesModel
+
+class StoriesAdapter (
+    private var _stories: MutableList<StoriesModel>, private val _listener: (StoriesModel) -> Unit
+) : RecyclerView.Adapter<StoriesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoriesViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.chip_item, parent, false)
+
+        return StoriesViewHolder(view)
+    }
+
+    override fun getItemCount() = _stories.size
+
+    override fun onBindViewHolder(holder: StoriesViewHolder, position: Int) {
+        val item = _stories[position]
+        holder.bind(item)
+        holder.itemView.setOnClickListener { _listener(item) }
+    }
+}
---
+++ MarvelApp/app/src/main/java/com/jenandsara/marvelapp/stories/viewmodel/StoriesViewModel.kt
@@ -0,0 +1,26 @@
+package com.jenandsara.marvelapp.stories.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.jenandsara.marvelapp.stories.model.StoriesModel
+import com.jenandsara.marvelapp.stories.repository.StoriesRepository
+import kotlinx.coroutines.Dispatchers
+
+
+class StoriesViewModel (val _repository: StoriesRepository) : ViewModel() {
+
+    private var _storiesList: List<StoriesModel> = listOf()
+
+    fun getStoriesList(id: Int) = liveData(Dispatchers.IO) {
+        val response = _repository.getStoriesById(id)
+        _storiesList = response.data.results
+        emit(response.data.results)
+    }
+
+    class StoriesViewModelFactory(private val _repository: StoriesRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return StoriesViewModel(_repository) as T
+        }
+    }
+}
