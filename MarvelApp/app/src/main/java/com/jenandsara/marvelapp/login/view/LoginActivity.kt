55f1a9039c0b0da58ecf047ea934929ed902657b
SARA ALVES <33603099+saraalves@users.noreply.github.com>
2021-01-06 21:34:08 -0300

    Feature/login social (#25)
    * login social
    
    * correção do layout de login
    
    * desenvolvimento de login social com o facebook

--- MarvelApp/app/src/main/java/com/jenandsara/marvelapp/login/view/LoginActivity.kt
+++ MarvelApp/app/src/main/java/com/jenandsara/marvelapp/login/view/LoginActivity.kt
@@ -1,21 +1,55 @@
 package com.jenandsara.marvelapp.login.view
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window.FEATURE_NO_TITLE
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.ktx.Firebase
 import com.jenandsara.marvelapp.R
 import com.jenandsara.marvelapp.cadastro.view.CadastroActivity
 import com.jenandsara.marvelapp.home.view.HomeActivity
+import com.jenandsara.marvelapp.login.AppUtil
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private val googleSignInClient: GoogleSignInClient? = null
+    private val imageFacebook: ImageView by lazy { findViewById<ImageView>(R.id.imgLoginFacebook) }
+    private lateinit var callbackManager: CallbackManager
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(FEATURE_NO_TITLE)
         setContentView(R.layout.activity_login)
+
+        callbackManager = CallbackManager.Factory.create()
+
+        imageFacebook.setOnClickListener { loginFacebook() }
+
+//        auth = Firebase.auth
 
         val buttonLogin = findViewById<TextView>(R.id.btnLogin)
         buttonLogin.setOnClickListener {
@@ -37,13 +71,6 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
 
-        val imageFacebook = findViewById<ImageView>(R.id.imgLoginFacebook)
-        imageFacebook.setOnClickListener {
-            Toast.makeText(this, "Logar com Facebook", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
 
         val imageGoogle = findViewById<ImageView>(R.id.imgLoginGoogle)
         imageGoogle.setOnClickListener {
@@ -54,4 +81,41 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
+
+     // começa as configs pro login social com o facebook
+
+    private fun irParaHome(uiid: String) {
+        AppUtil.salvarIdUsuario(application.applicationContext, uiid)
+        startActivity(Intent(applicationContext, HomeActivity::class.java))
+        finish()
+    }
+
+    private fun loginFacebook() {
+        val instanceFirebase = LoginManager.getInstance()
+
+        instanceFirebase.logInWithReadPermissions(this, listOf("email", "public_profile"))
+        instanceFirebase.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+
+            override fun onSuccess(loginResult: LoginResult) {
+                val credential: AuthCredential =
+                    FacebookAuthProvider.getCredential(loginResult.accessToken.token)
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+                    .addOnCompleteListener { irParaHome(loginResult.accessToken.userId) }
+            }
+
+            override fun onCancel() {
+                Toast.makeText(this@LoginActivity, "Cancelado!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onError(error: FacebookException) {
+                Toast.makeText(this@LoginActivity, error.message, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
