21d5e3213b1a2363558ca6ee0bd285df42547c3f
Sara Alves <saraalves14@hotmail.com>
2021-01-14 20:58:06 -0300

    Login com o google

--- MarvelApp/app/src/main/java/com/jenandsara/marvelapp/login/view/LoginActivity.kt
+++ MarvelApp/app/src/main/java/com/jenandsara/marvelapp/login/view/LoginActivity.kt
@@ -21,10 +21,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.AuthCredential
-import com.google.firebase.auth.FacebookAuthProvider
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
 import com.google.firebase.ktx.Firebase
 import com.jenandsara.marvelapp.R
 import com.jenandsara.marvelapp.cadastro.view.CadastroActivity
@@ -34,22 +31,33 @@ import com.jenandsara.marvelapp.login.AppUtil
 
 class LoginActivity : AppCompatActivity() {
 
-    private val googleSignInClient: GoogleSignInClient? = null
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
     private val imageFacebook: ImageView by lazy { findViewById<ImageView>(R.id.imgLoginFacebook) }
     private lateinit var callbackManager: CallbackManager
 
-    private lateinit var auth: FirebaseAuth
+    private val imageGoogle: ImageView by lazy { findViewById<ImageView>(R.id.imgLoginGoogle) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(FEATURE_NO_TITLE)
         setContentView(R.layout.activity_login)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        auth = FirebaseAuth.getInstance()
+
         callbackManager = CallbackManager.Factory.create()
 
         imageFacebook.setOnClickListener { loginFacebook() }
 
-//        auth = Firebase.auth
+        imageGoogle.setOnClickListener { signIn() }
 
         val buttonLogin = findViewById<TextView>(R.id.btnLogin)
         buttonLogin.setOnClickListener {
@@ -70,16 +78,6 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-
-
-        val imageGoogle = findViewById<ImageView>(R.id.imgLoginGoogle)
-        imageGoogle.setOnClickListener {
-            Toast.makeText(this, "Logar com Google", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
     }
 
      // começa as configs pro login social com o facebook
@@ -113,9 +111,80 @@ class LoginActivity : AppCompatActivity() {
         })
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        callbackManager.onActivityResult(requestCode, resultCode, data)
+    // configurações pra login no google
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception = task.exception
+            if (task.isSuccessful) {
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d("MainActivity", "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("MainActivity", "Google sign in failed", e)
+                    // ...
+                }
+            } else {
+                Log.w("MainActivity", exception.toString())
+
+            }
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            if (currentUser.isEmailVerified) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Valide seu email", Toast.LENGTH_LONG).show()
+                auth.signOut()
+            }
+        }
+
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithCredential:failure", task.exception)
+                    // ...
+                }
+            }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 1
+    }
 }
