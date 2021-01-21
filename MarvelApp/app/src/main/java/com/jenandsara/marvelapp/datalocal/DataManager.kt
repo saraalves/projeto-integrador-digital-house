21aec9ebf5fc68d2a289f21de5a5e9d9f56bc8a5
Sara Alves <saraalves14@hotmail.com>
2021-01-20 23:10:37 -0300

    criação da dataManager para armazenar os dados do room

--- MarvelApp/app/src/main/java/com/jenandsara/marvelapp/datalocal/DataManager.kt
+++ MarvelApp/app/src/main/java/com/jenandsara/marvelapp/datalocal/DataManager.kt
@@ -12,13 +12,17 @@ open class DataManager(
     private val sharedPreferencesEdit = getSharedPrefs(application).edit()
 
     companion object {
-        const val MARVEL_PREFERENCES = "dfm_preferences"
+        const val MARVEL_PREFERENCES = "marvel_preferences"
     }
 
     suspend fun adicionarCharacter(character: List<CharacterEntity>) = characterDao?.adicionarCharacter(character)
     suspend fun atualizarIsFavorite(int: Int, id_api: Int) = characterDao?.atualizarIsFavorite(int, id_api)
     suspend fun obterTodos(): List<CharacterEntity>? = characterDao?.obterTodos()
     suspend fun obterFavoritos(): List<CharacterEntity>? = characterDao?.obterFavoritos()
+
+    suspend fun salvarCharacter(character: List<CharacterEntity>, isSuccess: (Boolean) -> Unit) {
+        characterDao.adicionarCharacter(character)
+    }
 
     fun getSharedPrefs(androidApplication: Application): SharedPreferences =
         androidApplication.getSharedPreferences(MARVEL_PREFERENCES, android.content.Context.MODE_PRIVATE)
