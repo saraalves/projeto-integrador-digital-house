7000bc5774471a4725dec93eea0d5dee29cef7d4
Sara Alves <saraalves14@hotmail.com>
2021-01-23 20:19:22 -0300

    salvando os dados com o room

--- MarvelApp/app/src/main/java/com/jenandsara/marvelapp/home/view/character/CharacterViewHolder.kt
+++ MarvelApp/app/src/main/java/com/jenandsara/marvelapp/home/view/character/CharacterViewHolder.kt
@@ -12,47 +12,17 @@ import com.jenandsara.marvelapp.R
 import com.jenandsara.marvelapp.character.model.CharacterModel
 import com.squareup.picasso.Picasso
 
-class CharacterViewHolder(view: View, private val context: Context) : RecyclerView.ViewHolder(view) {
+class CharacterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val imagem = view.findViewById<ImageView>(R.id.cardPersonagem)
     private val nome = view.findViewById<TextView>(R.id.txtNamePersongemCard)
-    private val favorite = view.findViewById<MaterialButton>(R.id.btnFavoritar)
-    private val constraintFavorite = view.findViewById<ConstraintLayout>(R.id.ctlCardPersonagem)
 
-    interface OnCharacterClickListener {
-        fun onCharacterClick(position: Int)
-        fun onCharacterFavoriteClick(position: Int)
-    }
-
-    fun bind(character: CharacterModel, onCharacterClickListener: OnCharacterClickListener) {
+    fun bind(character: CharacterModel) {
         nome.text = character.nome
 
         val imagePath = character.thumbnail?.getImagePath()
         Picasso.get()
             .load(imagePath)
             .into(imagem)
-
-        if (character.isFavorite) {
-            favorite.background = ContextCompat.getDrawable(
-                context,
-                R.drawable.ic_favorite_red_24
-            )
-
-            constraintFavorite.setBackgroundColor(
-                ContextCompat.getColor(
-                    context,
-                    R.color.colorPrimary
-                )
-            )
-        } else {
-            favorite.background = ContextCompat.getDrawable(
-                context,
-                R.drawable.ic_favorite_gray_24
-            )
-        }
-
-        favorite.setOnClickListener {
-            onCharacterClickListener.onCharacterFavoriteClick(adapterPosition)
-        }
     }
 }
