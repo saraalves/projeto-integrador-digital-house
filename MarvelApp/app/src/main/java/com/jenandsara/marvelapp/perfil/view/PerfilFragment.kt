49228f3e7b8c6fd357eb8aa3e3623b6ba572bfc4
Jenifer Soares <jey.souza11@gmail.com>
2021-01-23 14:31:30 -0300

    Pega informçãoes do usuário

--- MarvelApp/app/src/main/java/com/jenandsara/marvelapp/perfil/view/PerfilFragment.kt
+++ MarvelApp/app/src/main/java/com/jenandsara/marvelapp/perfil/view/PerfilFragment.kt
@@ -6,16 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.jenandsara.marvelapp.alterarsenha.view.AlterarSenhaActivity
 import com.jenandsara.marvelapp.splashscreen.view.SplashScreenActivity
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.button.MaterialButtonToggleGroup
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.jenandsara.marvelapp.R
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 
 class PerfilFragment : Fragment() {
     override fun onCreateView(
@@ -46,17 +48,40 @@ class PerfilFragment : Fragment() {
 
         val toggleNome = view.findViewById<MaterialButtonToggleGroup>(R.id.toggleNome)
         toggleNome.addOnButtonCheckedListener { _, _, isChecked ->
-            if(isChecked) {
-                view.findViewById<TextInputLayout>(R.id.txtNomePerfil).isEnabled = true
-            } else view.findViewById<TextInputLayout>(R.id.txtNomePerfil).isEnabled = false
+                view.findViewById<TextInputLayout>(R.id.txtNomePerfil).isEnabled = isChecked
         }
 
         val toggleEmail = view.findViewById<MaterialButtonToggleGroup>(R.id.toggleEmail)
         toggleEmail.addOnButtonCheckedListener { _, _, isChecked ->
-            if(isChecked) {
-                view.findViewById<TextInputLayout>(R.id.txtEmailPerfil).isEnabled = true
-            } else  view.findViewById<TextInputLayout>(R.id.txtEmailPerfil).isEnabled = false
+                view.findViewById<TextInputLayout>(R.id.txtEmailPerfil).isEnabled = isChecked
         }
 
+        getInfo(view)
+
     }
+
+    private fun getInfo(view: View){
+
+        var nomePerfil = view.findViewById<TextInputEditText>(R.id.etNomeAtual)
+        var emailPerfil = view.findViewById<TextInputEditText>(R.id.etEmailAtual)
+        val imgPerfil = view.findViewById<CircleImageView>(R.id.imgPhotoPerfil)
+
+        val user = Firebase.auth.currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+            val photoUrl = user.photoUrl
+            val uid = user.uid
+
+            nomePerfil.setText(name)
+            emailPerfil.setText(email)
+
+            if(photoUrl != null){
+                Picasso.get().load(photoUrl).into(imgPerfil)
+            }
+        }
+    }
+
+
 }
