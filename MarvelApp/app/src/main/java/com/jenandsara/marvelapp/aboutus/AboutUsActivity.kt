c3907da123896730b9af9824d84aae5f8df2b58a
SARA ALVES <33603099+saraalves@users.noreply.github.com>
2021-02-10 17:41:05 -0300

    Fix/clean code (#37)
    * ajuste no about us
    
    * ajuste no layout de perfil
    
    * usando o lint
    
    * Ajuste layout
    
    Co-authored-by: Jenifer Soares <jey.souza11@gmail.com>

--- MarvelApp/app/src/main/java/com/jenandsara/marvelapp/aboutus/AboutUsActivity.kt
+++ MarvelApp/app/src/main/java/com/jenandsara/marvelapp/aboutus/AboutUsActivity.kt
@@ -2,6 +2,9 @@ package com.jenandsara.marvelapp.aboutus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import android.widget.Toolbar
 import com.google.android.material.appbar.MaterialToolbar
 import com.jenandsara.marvelapp.R
 
@@ -10,12 +13,20 @@ class AboutUsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
 
-        val toolbar = findViewById<MaterialToolbar>(R.id.toolbarAboutUs)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbarAboutUs)
 
-        toolbar.setOnClickListener{
+        toolbar.setOnClickListener {
             onBackPressed()
             finish()
         }
 
+        setupHyperlink()
+    }
+
+    private fun setupHyperlink() {
+        val linkGithubJenn = findViewById<TextView>(R.id.txtGithubJenn)
+        val linkGithubSara = findViewById<TextView>(R.id.txtGithubSara)
+        linkGithubJenn.movementMethod = LinkMovementMethod.getInstance()
+        linkGithubSara.movementMethod = LinkMovementMethod.getInstance()
     }
 }
