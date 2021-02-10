e74b82f5fd80e2047cf2783907fca91b5aca6cd2
Sara Alves <saraalves14@hotmail.com>
2021-02-09 21:48:52 -0300

    ajuste no about us

--- MarvelApp/app/src/main/java/com/jenandsara/marvelapp/aboutus/AboutUsActivity.kt
+++ MarvelApp/app/src/main/java/com/jenandsara/marvelapp/aboutus/AboutUsActivity.kt
@@ -2,6 +2,8 @@ package com.jenandsara.marvelapp.aboutus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import com.google.android.material.appbar.MaterialToolbar
 import com.jenandsara.marvelapp.R
 
@@ -12,10 +14,18 @@ class AboutUsActivity : AppCompatActivity() {
 
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbarAboutUs)
 
-        toolbar.setOnClickListener{
+        toolbar.setOnClickListener {
             onBackPressed()
             finish()
         }
 
+        setupHyperlink()
+    }
+
+    private fun setupHyperlink() {
+        val linkGithubJenn = findViewById<TextView>(R.id.txtGithubJenn)
+        val linkGithubSara = findViewById<TextView>(R.id.txtGithubSara)
+        linkGithubJenn.movementMethod = LinkMovementMethod.getInstance()
+        linkGithubSara.movementMethod = LinkMovementMethod.getInstance()
     }
 }
